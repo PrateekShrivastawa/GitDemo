7f82a33093076bc5237fa1237c4d8025e64af66a
Prateek Shrivastawa <prateek.shrivastawa_cs18@gla.ac.in>
2024-02-02 20:29:18 +0530

    Added Bye

--- helloworld.java
+++ helloworld.java
@@ -3,5 +3,7 @@ package gittutorial;
 public class helloworld{
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Bye");
     }
 }
+
